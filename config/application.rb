04a174f4d0ea0f7637e58d0d41175667dd53dd95
evanurafny <evanurafnykh@gmail.com>
2025-05-24 08:34:27 +0700

    Fix ActiveRecord cache DB lookup in production

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module ChatAppEva
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
+    config.active_record.database_selector = nil
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
